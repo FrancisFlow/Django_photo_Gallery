7c9d963b9ce8c8112e7ff1868bc8255d8d937295
FrancisFlow <iamngigikaranja1@aol.com>
2022-03-28 00:50:27 +0300

    Defined view function for search by category

--- gallery/views.py
+++ gallery/views.py
@@ -6,3 +6,13 @@ def home(request):
     images=Image.get_images()
 
     return render(request, 'home.html', {"images":images})
+
+def search_by_category(request):
+    if 'image_category' in request.GET and request.GET["image_category"]:
+        search_term=request.GET.get("image_category")
+        searched_images=Image.search_by_category(search_term)
+        return render(request, 'search.html', {"images":searched_images})
+    
+    else:
+        message="No search term added"
+        return render(request, 'search.html', {"message":message})
