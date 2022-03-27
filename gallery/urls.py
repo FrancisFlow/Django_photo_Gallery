a7fd9ae2a851953ac918081bd52e3c06df48c66d
FrancisFlow <iamngigikaranja1@aol.com>
2022-03-28 00:53:15 +0300

    Added search url to gallery urlpatterns

--- gallery/urls.py
+++ gallery/urls.py
@@ -4,7 +4,8 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns=[
-    path('', views.home, name = 'home')
+    path('', views.home, name = 'home'),
+    path('search/', views.search_by_category, name='search_by_category')
 
 ]
 
