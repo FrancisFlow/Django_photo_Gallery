663a841b6e6a58929c25c123bf22066f90f09df7
FrancisFlow <iamngigikaranja1@aol.com>
2022-03-29 08:17:22 +0300

    Created unitests for Location class

--- gallery/tests.py
+++ gallery/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
-
+from .models import Image, Location, Category
 # Create your tests here.
+
+class LocationTestClass(TestCase):
+    def setUp(self):
+        self.sweden=Location(name='Sweden')
+    
+    def test_instance(self):
+        self.assertTrue(isinstance(self.sweden, 'Sweden'))
+    def test_save_method(self):
+        self.sweden.save_location()
+        locations=Location.objects.all()
+        self.assertTrue(len(locations)>0)
