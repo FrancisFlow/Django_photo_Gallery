38352b01ea9d996117ccc8aa643d4ab6e917597f
FrancisFlow <iamngigikaranja1@aol.com>
2022-03-29 08:00:43 +0300

    Modified the max-height and width of the images

--- gallery/urls.py
+++ gallery/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
+from django.conf.urls import url
 from . import views
 from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns=[
     path('', views.home, name = 'home'),
-    path('search/', views.search_by_category, name='search_by_category')
-    path('locations/', views.photos_by_location, name='photos_by_location')
+    path('search/', views.search_by_category, name='search_by_category'),
+    url(r'^locations/(\d+)', views.photos_by_location, name='photosByLocation')
 
 ]
 
