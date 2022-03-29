053d1fbd633a104a55acc0165720643b15daba19
FrancisFlow <iamngigikaranja1@aol.com>
2022-03-29 08:21:09 +0300

    Created unittests for Category

--- gallery/tests.py
+++ gallery/tests.py
@@ -12,3 +12,15 @@ class LocationTestClass(TestCase):
         self.sweden.save_location()
         locations=Location.objects.all()
         self.assertTrue(len(locations)>0)
+    
+
+class CategoryTestClass(TestCase):
+    def setUp(self):
+        self.anime=Category(name='Anime')
+    def test_instance(self):
+        self.assertTrue(isinstance(self.anime.name, 'Anime'))
+    def test_save_method(self):
+        self.anime.save_category()
+        categories=Category.objects.all()
+        self.assertTrue(len(categories)>0)
+    
