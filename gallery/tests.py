a155c6fe9bd136b313eac8cced90c59c3c7a43e4
FrancisFlow <iamngigikaranja1@aol.com>
2022-03-29 08:27:54 +0300

    Created unittests for Image class and its methods

--- gallery/tests.py
+++ gallery/tests.py
@@ -23,4 +23,32 @@ class CategoryTestClass(TestCase):
         self.anime.save_category()
         categories=Category.objects.all()
         self.assertTrue(len(categories)>0)
+ 
+ class ImageTestClass(TestCase):
+
+     def setUp(self):
+         self.anime=Category(name='Anime')
+         self.anime.save_category()
+
+         self.location=Location(name='Sweden')
+         self.location.save_location()
+
+         self.new_image=Image(image_name='Kaizen', image_description='The supreme masters of ninja', image_category=selr.anime, image_location=self.location)
+         self.new_image.save_image()
+
+    def tearDown(self):
+        Category.objects.all().delete()
+        Location.objects.all().delete()
+        Image.objects.all().delete()
+    
+    
+    def test_get_images(self):
+        all_images = Image.get_images()
+        self.assertTrue(len(all_images)>0)
+     
+    def test_filter_by_location(self):
+        test_location_id = 6
+        images_location = Image.filter_by_location(test_location_id) 
+        self.assertTrue(len(images_location) == 0)  
+
     
